1912c556e6b4e162e68e3561c017170f89f7206f
marcoroncuzzi02 <marcoroncuzzi02@gmail.com>
2025-03-25 23:34:27 +0100

    ADD cors

--- app.js
+++ app.js
@@ -6,26 +6,25 @@ const app = express()
 
 const port = process.env.SERVER_PORT || 3005;
 
+import cors from 'cors'
+
 import movieRouter from './routers/movieRouter.js'
 
 import ImagePath from './middlewares/imagePath.js';
 
-// const endPointMiddleware = require('./middlewares/endPointMiddleware')
-// const errorHandler = require('./middlewares/errorHandler')
+app.use(cors({
+    origin : process.env.FRONTEND_APP,
+}))
 
 app.use(express.static('../public'));
 
 app.use(express.json())
 
-// app.get('/movies', (req, res) => {
-// res.send('QUESTO è IL SERVER DI MOVIES')
-// })
 
 app.use("/movies", movieRouter)
 
 app.use( ImagePath )
-// app.use(endPointMiddleware)
-// app.use(errorHandler)
+
 
 app.listen(port, () => {
 console.log(`Example app listening on port ${port}`)
